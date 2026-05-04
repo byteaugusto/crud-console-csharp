7de3d38704f81f1ea94e4080ae719cdcbadfc1e2
byteaugusto <mateusaugustoservo@outlook.com>
2026-05-04 13:59:36 -0300

    sistema crud

--- Program.cs
+++ Program.cs
@@ -11,19 +11,11 @@ namespace Crud
             int i = 0;
             string caminho = "nomes.txt";
             List<Nomes> list = new List<Nomes>();
-            if (File.Exists(caminho))
-            {
-                string[] linhas = File.ReadAllLines(caminho);
-                foreach (string linha in linhas)
-                {
-                    list.Add(new Nomes { Nome = linha });
-                }
-            }
+            ArquivoRepository.Carregar(list, caminho);
 
             while (i != 6)
             {
-                Console.WriteLine("1- cadastrar novo nome  2- remover nome da lista 3- mostrar nomes na lista 4- editar nome na lista -6- sair  ");
-                Console.WriteLine("digite uma opção");
+                Console.WriteLine("===== MENU =====\r\n1 - Cadastrar nome\r\n2 - Remover nome\r\n3 - Listar nomes\r\n4 - Editar nome\r\n6 - Sair\r\n================\r\nEscolha uma opção:");
                 if (!int.TryParse(Console.ReadLine(), out i))
                 {
                     Console.WriteLine("Digite um número válido!");
@@ -33,14 +25,15 @@ namespace Crud
                 {
                     case 1:
                         Nomes nomes = new Nomes();
-                        Console.WriteLine("Digite um nome: ");
+                        Console.WriteLine("=== CADASTRAR NOME ===\r\nDigite um nome: ");
                         nomes.Nome = Console.ReadLine();
                         list.Add(nomes);
-                        Salvar(list, caminho);
-                        for (int indice = 0; indice < list.Count; indice++)
-                        {
-                            Console.WriteLine(indice + " - " + list[indice].Nome);
-                        }
+                        ArquivoRepository.Salvar(list, caminho);
+                        NomeIndice(list);
+                        Console.WriteLine("Nome cadastrado com sucesso!");
+                        Console.WriteLine("Pressione qualquer tecla para continuar...");
+                        Console.ReadKey();
+                        Console.Clear();
                         break;
                         case 2:
                         int remove = 0;
@@ -48,48 +41,71 @@ namespace Crud
                         if (list.Count == 0)
                         {
                             Console.WriteLine("não há nada na lista");
+                            Console.WriteLine("Pressione qualquer tecla para continuar...");
+                            Console.ReadKey();
+                            Console.Clear();
                             break;
                         }
                         else
                         {
-                            for (int indice = 0; indice < list.Count; indice++)
-                            {
-                                Console.WriteLine(indice + " - " + list[indice].Nome);
-                            }
-                            Console.WriteLine("qual nome deseja remover?");
-                            int.TryParse(Console.ReadLine(), out remove);
-                            if (remove >= 0 && remove < list.Count)
-                            {
+                            Console.WriteLine("=== REMOVER NOME ===");
+                            NomeIndice(list);
+                            Console.Write("Digite o índice do nome que deseja remover: ");
+                                if (!int.TryParse(Console.ReadLine(), out remove))
+                                {
+                                    Console.WriteLine("Entrada inválida!");
+                                    Console.WriteLine("Pressione qualquer tecla para continuar...");
+                                    Console.ReadKey();
+                                    Console.Clear();
+                                    break;
+                                }
+                                if (remove >= 0 && remove < list.Count)
+                                {
                                 Console.WriteLine("tem certeza que quer remover este nome? 1- sim 2- não");
                                 int.TryParse(Console.ReadLine(), out numeroremocao);
                                 if (numeroremocao == 1)
                                 {
                                     list.RemoveAt(remove);
-                                    Salvar(list, caminho);
+                                    ArquivoRepository.Salvar(list, caminho);
+                                    Console.WriteLine("Nome removido!");
+                                    Console.WriteLine("Pressione qualquer tecla para continuar...");
+                                    Console.ReadKey();
+                                    Console.Clear();
                                 }
                                 else
                                 {
                                     Console.WriteLine("tente novamente");
+                                    Console.WriteLine("Pressione qualquer tecla para continuar...");
+                                    Console.ReadKey();
+                                    Console.Clear();
                                 }
                             }
                             else
                             {
                                 Console.WriteLine("insira um número dentro do intervalo de indices");
+                                Console.WriteLine("Pressione qualquer tecla para continuar...");
+                                Console.ReadKey();
+                                Console.Clear();
                             }
                         }
                         break;
                     case 3:
                         if (list.Count == 0)
                         {
+                            Console.WriteLine("=== LISTAR NOMES ===");
                             Console.WriteLine("não há nada na lista");
+                            Console.WriteLine("Pressione qualquer tecla para continuar...");
+                            Console.ReadKey();
+                            Console.Clear();
                             break;
                         }
                         else
                         {
-                            for (int indice = 0; indice < list.Count; indice++)
-                            {
-                                Console.WriteLine(indice + " - " + list[indice].Nome);
-                            }
+                            Console.WriteLine("=== LISTAR NOMES ===");
+                            NomeIndice(list);
+                            Console.WriteLine("Pressione qualquer tecla para continuar...");
+                            Console.ReadKey();
+                            Console.Clear();
                         }
                         break;
                     case 4:
@@ -99,57 +115,78 @@ namespace Crud
                         if (list.Count == 0)
                         {
                             Console.WriteLine("não há nada na lista");
+                            Console.WriteLine("Pressione qualquer tecla para continuar...");
+                            Console.ReadKey();
+                            Console.Clear();
                             break;
                         }
                         else
                         {
-                            for (int indice = 0; indice < list.Count; indice++)
-                            {
-                                Console.WriteLine(indice + " - " + list[indice].Nome);
+                            NomeIndice(list);
+                            Console.WriteLine("=== EDITAR NOME ===");
+                            Console.Write("Digite o índice do nome que deseja editar: ");
+                            if (!int.TryParse(Console.ReadLine(), out edit))
+                            {                                
+                                    Console.WriteLine("Entrada inválida!");
+                                    Console.WriteLine("Pressione qualquer tecla para continuar...");
+                                    Console.ReadKey();
+                                    Console.Clear();
+                                    break;                                
                             }
-                            Console.WriteLine("qual nome deseja editar?");
-                            int.TryParse(Console.ReadLine(), out edit);
                             if (edit >= 0 && edit < list.Count)
-                            {
+                                {
                                 Console.WriteLine("tem certeza que quer editar este nome? 1- sim 2- não");
-                                int.TryParse(Console.ReadLine(), out numeroedit);
+                                if (!int.TryParse(Console.ReadLine(), out numeroedit))
+                                {
+                                    Console.WriteLine("Entrada inválida!");
+                                    Console.WriteLine("Pressione qualquer tecla para continuar...");
+                                    Console.ReadKey();
+                                    Console.Clear();
+                                    break;
+                                }
                                 if (numeroedit == 1)
                                 {
                                     Console.WriteLine("digite o novo nome");
                                     novonome = Console.ReadLine();
                                     list[edit].Nome = novonome;
-                                    Salvar(list, caminho);
+                                    ArquivoRepository.Salvar(list, caminho);
+                                    Console.WriteLine("Nome atualizado!");
+                                    Console.WriteLine("Pressione qualquer tecla para continuar...");
+                                    Console.ReadKey();
+                                    Console.Clear();
+
                                 }
                                 else
                                 {
                                     Console.WriteLine("tente novamente");
+                                    Console.WriteLine("Pressione qualquer tecla para continuar...");
+                                    Console.ReadKey();
+                                    Console.Clear();
                                 }
                             }
                             else
                             {
                                 Console.WriteLine("insira um número dentro do intervalo de indices");
+                                Console.WriteLine("Pressione qualquer tecla para continuar...");
+                                Console.ReadKey();
+                                Console.Clear();
                             }
                             break;
                         }
                     default:
                         Console.WriteLine("opção invalida");
+                        Console.WriteLine("Pressione qualquer tecla para continuar...");
+                        Console.ReadKey();
+                        Console.Clear();
                         break;
                 }
              }
-        }
-        public class Nomes { 
-        public string Nome {  get; set; }
-        }
-        static void Salvar(List<Nomes> list, string caminho)
+        }static void NomeIndice(List<Nomes> list)
         {
-            List<string> nomes = new List<string>();
-
-            foreach (var item in list)
+            for (int indice = 0; indice < list.Count; indice++)
             {
-                nomes.Add(item.Nome);
+                Console.WriteLine(indice + " - " + list[indice].Nome);
             }
-
-            File.WriteAllLines(caminho, nomes);
         }
     }
 }
---
+++ Repositorio.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Crud
+{
+    public class ArquivoRepository
+    {
+        public static void Salvar(List<Nomes> list, string caminho)
+        {
+        List<string> linhas = new List<string>();
+            foreach (Nomes nome in list)
+            {
+                linhas.Add(nome.Nome);
+            }
+        File.WriteAllLines(caminho, linhas);
+    }
+        public static void Carregar(List<Nomes> list, string caminho)
+        {
+            if (File.Exists(caminho))
+            {
+                string[] linhas = File.ReadAllLines(caminho);
+                foreach (string linha in linhas)
+                {
+                    list.Add(new Nomes { Nome = linha });
+                }
+            }
+        }
+    }
+}
